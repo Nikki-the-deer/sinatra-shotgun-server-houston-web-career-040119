8dc9c458866369b397b72e3fd4440f6891f6514f
Nikki-the-deer <nick-2013@live.com>
2019-04-22 14:20:54 -0500

    yeet

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! Here's to a new chapter. This is a test of the shotgun gem for Ruby, the programming language "
   end
 
 end
